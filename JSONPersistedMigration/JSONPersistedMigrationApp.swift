e0c9927849b0d5001b7f5c5bcd448a1d03ef9fc2
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-18 23:20:27 +0100

    Blank project

---
+++ JSONPersistedMigration/JSONPersistedMigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONPersistedMigrationApp.swift
+//  JSONPersistedMigration
+//
+//  Created by Javier Calatrava on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct JSONPersistedMigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JSONPersistedMigrationTests/JSONPersistedMigrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSONPersistedMigrationTests.swift
+//  JSONPersistedMigrationTests
+//
+//  Created by Javier Calatrava on 18/12/24.
+//
+
+import Testing
+@testable import JSONPersistedMigration
+
+struct JSONPersistedMigrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
